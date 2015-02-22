5d28a1d3f8fbaab0bc47034e86b8e2c810b9a562
Carlos Fernandez <cfern1990@gmail.com>
2015-02-21 20:21:42 -0500

    Initial Commit

---
+++ DrawingShareDesktop/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Shell;
+
+namespace DrawingShareDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // allow resizing the window from all sides
+            var chrome = new WindowChrome();
+            chrome.ResizeBorderThickness = mainContainer.Margin;
+            chrome.GlassFrameThickness = new Thickness(0);
+            chrome.CaptionHeight = 0;
+            WindowChrome.SetWindowChrome(this, chrome);
+        }
+
+        private void mainContainer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void chrome_closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void chrome_minimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void chrome_maximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+    }
+}
